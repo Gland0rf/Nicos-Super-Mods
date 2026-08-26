7b53495949d35dbdc5e77e83732dd8538058c03f
Gland0rf <ndkogler@icloud.com>
2026-08-26 15:49:55 +0300

    Fixed error where door coords would get rounded and sometimes produce an offset of +-1 blocks

--- src/client/java/com/nico/client/bloodrush/DoorId.java
+++ src/client/java/com/nico/client/bloodrush/DoorId.java
@@ -5,18 +5,12 @@ import net.minecraft.world.phys.Vec3;
 
 public record DoorId (int x, int y, int z) {
     public static DoorId fromWorld(BlockPos doorAnchor, RoomInfo room) {
-        Vec3 worldCenter = new Vec3(
-                doorAnchor.getX() + 0.5,
-                doorAnchor.getY() + 0.5,
-                doorAnchor.getZ() + 0.5
-        );
-
-        Vec3 local = RouteTransforms.worldToLocal(worldCenter, room);
+        BlockPos local = RouteTransforms.worldBlockToLocal(doorAnchor, room);
 
         return new DoorId(
-                (int) Math.round(local.x),
-                (int) Math.round(local.y),
-                (int) Math.round(local.z)
+                local.getX(),
+                local.getY(),
+                local.getZ()
         );
     }
 
