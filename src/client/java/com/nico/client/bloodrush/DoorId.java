bd4a2e81fd77bfc59fb4986ea55b02a1d462c095
Gland0rf <ndkogler@icloud.com>
2026-08-29 23:27:02 +0300

    Fixed door offset

--- src/client/java/com/nico/client/bloodrush/DoorId.java
+++ src/client/java/com/nico/client/bloodrush/DoorId.java
@@ -4,6 +4,8 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.phys.Vec3;
 
 public record DoorId (int x, int y, int z) {
+    public static final int TOLERANCE = 1;
+
     public static DoorId fromWorld(BlockPos doorAnchor, RoomInfo room) {
         BlockPos local = RouteTransforms.worldBlockToLocal(doorAnchor, room);
 
@@ -13,6 +15,21 @@ public record DoorId (int x, int y, int z) {
                 local.getZ()
         );
     }
+
+    public boolean matchesWithTolerance(DoorId other) {
+        return other != null
+                && y == other.y
+                && Math.abs(x - other.x) <= TOLERANCE
+                && Math.abs(z - other.z) <= TOLERANCE;
+    }
+
+   public int xzDistanceTo(DoorId other) {
+        if (other == null || y != other.y) {
+             return Integer.MAX_VALUE;
+        }
+
+        return Math.abs(x - other.x) + Math.abs(z - other.z);
+   }
 
     public String fileName() {
         return x + "_" + y + "_" + z;
